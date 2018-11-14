056bedce6e5f3a851e05eb6463115f840efe5516
oracle <oracle@localhost.localdomain>
2018-11-14 19:46:46 +0100

    algo

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -1,38 +1,26 @@
-
 package factorial;
-
 
 public class Factorial {
 
-
     public static void main(String[] args) {
 
+        int j;
+        int f;
 
-    int j;
-    int f;
+        j = 8;
 
-  
-   j = 8;    
-   
+        int i;
+        if (j == 0) {
+            f = 1;
+        } else {
+            f = 1;
+            for (i = j; i >= 1; i--) {
+                f = f * i;
+            }
+        }
 
-    
-   int i;
-   if (j == 0)
-f = 1;
-   else
-   {
-f = 1;
-for (i = j; i >= 1; i--)
-{
-    f = f * i;
-}
-   }    
-  
- 
-
-System.out.println(f); 
-
+        System.out.println(f);
 
     }
-    
+
 }
